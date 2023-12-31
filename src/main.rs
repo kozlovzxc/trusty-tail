89bed5939846995d7f508e13c608e47faac6d20c
Nikita <kozlovzxc@gmail.com>
2023-12-31 17:25:50 +0100

    added emergency text fetcher

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ mod config;
 
 use config::Config;
 use entity::{prelude::*, *};
-use log::error;
 use migration::Migrator;
 use sea_orm::{ActiveValue, ColumnTrait, Database, DatabaseConnection, EntityTrait, QueryFilter};
 use sea_orm_migration::prelude::*;
 use std::error::Error;
-use std::sync::Arc;
-use teloxide::{dispatching::dialogue::InMemStorage, prelude::*, utils::command::BotCommands};
+use std::fmt::{self, Debug};
+use teloxide::dispatching::dialogue::InMemStorage;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
-#[derive(BotCommands, Clone)]
+#[derive(BotCommands, Clone, PartialEq, Eq)]
 #[command(
     rename_rule = "lowercase",
     description = "These commands are supported:"
@@ -19,22 +19,116 @@ enum Command {
     Start,
     #[command(description = "display this text.")]
     Help,
+    TestError,
     #[command(description = "Add text regarding what to do in case of emergency")]
     UpdateEmergencyText,
     #[command(description = "Get text regarding what to do in case of emergency")]
     GetEmergencyText,
 }
 
-#[derive(Clone, Default)]
-enum DialogState {
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+enum BotDialogState {
     #[default]
-    Start,
-    AwaitingAge,
-    AwaitingName,
-    AwaitingCity,
+    Idle,
+    WaitingEmergencyText,
 }
 
-type MyDialogue = Dialogue<DialogState, InMemStorage<DialogState>>;
+type BotDialogue = Dialogue<BotDialogState, InMemStorage<BotDialogState>>;
+
+type HandlerResult = Result<(), Box<dyn Error + Send + Sync>>;
+
+async fn print_start_info(bot: Bot, message: Message, dialogue: BotDialogue) -> HandlerResult {
+    dialogue.exit().await?;
+    bot.send_message(message.chat.id, "Start!").await?;
+    Ok(())
+}
+
+#[derive(Debug)]
+pub enum TestError {
+    SimpleError(String),
+}
+
+impl fmt::Display for TestError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            TestError::SimpleError(ref msg) => write!(f, "{}", msg),
+        }
+    }
+}
+
+impl Error for TestError {}
+
+async fn trigger_error() -> HandlerResult {
+    Err(Box::new(TestError::SimpleError("Simple error".into())))
+}
+
+async fn print_help_info(bot: Bot, message: Message, dialogue: BotDialogue) -> HandlerResult {
+    dialogue.exit().await?;
+    bot.send_message(message.chat.id, "Help!").await?;
+    Ok(())
+}
+
+async fn ask_for_emergency_info(
+    bot: Bot,
+    message: Message,
+    dialogue: BotDialogue,
+) -> HandlerResult {
+    dialogue
+        .update(BotDialogState::WaitingEmergencyText)
+        .await?;
+    bot.send_message(message.chat.id, "Input your emergency text!")
+        .await?;
+    Ok(())
+}
+
+async fn update_emergency_info(
+    bot: Bot,
+    message: Message,
+    dialogue: BotDialogue,
+    connection: DatabaseConnection,
+) -> HandlerResult {
+    dialogue.exit().await?;
+    let new_emergency_info = emergency_info::ActiveModel {
+        text: ActiveValue::Set(message.text().unwrap_or("").to_string()),
+        chat_id: ActiveValue::Set(message.chat.id.0),
+        ..Default::default()
+    };
+    EmergencyInfo::insert(new_emergency_info)
+        .on_conflict(
+            OnConflict::column(emergency_info::Column::ChatId)
+                .update_column(emergency_info::Column::Text)
+                .to_owned(),
+        )
+        .exec(&connection)
+        .await?;
+
+    bot.send_message(message.chat.id, "Updated!").await?;
+    Ok(())
+}
+
+async fn get_emergency_info(
+    bot: Bot,
+    message: Message,
+    dialogue: BotDialogue,
+    connection: DatabaseConnection,
+) -> HandlerResult {
+    dialogue.exit().await?;
+    let emergency_info = EmergencyInfo::find()
+        .filter(emergency_info::Column::ChatId.eq(message.chat.id.0))
+        .one(&connection)
+        .await?;
+    match emergency_info {
+        Some(emergency_info) => {
+            bot.send_message(message.chat.id, emergency_info.text)
+                .await?;
+        }
+        None => {
+            bot.send_message(message.chat.id, "There is no saved emergency info")
+                .await?;
+        }
+    }
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -47,79 +141,59 @@ async fn main() -> Result<(), Box<dyn Error>> {
         config.db_user, config.db_password, config.db_url, config.db_name
     );
     let connection = Database::connect(database_full_url).await?;
-    log::info!("Connected to dabase...");
+    log::info!("Connected to database...");
     let schema_manager = SchemaManager::new(&connection);
     Migrator::up(&connection, None).await?;
     assert!(schema_manager.has_table("emergency_info").await?);
     log::info!("Applied migrations...");
 
     let bot = Bot::from_env();
-    let connection = Arc::new(connection);
 
     let handler = Update::filter_message()
+        .enter_dialogue::<Message, InMemStorage<BotDialogState>, BotDialogState>()
+        .map(|message: Message| {
+            let text = message.text().unwrap_or_default();
+            Command::parse(&text, "").ok()
+        })
+        .map_async(|dialogue: BotDialogue| async move { dialogue.get().await.ok().flatten() })
         .branch(
-            dptree::entry()
-                .filter_map(|msg: Message| Command::parse(&msg.text().unwrap_or_default(), "").ok())
-                .branch(dptree::case![Command::Start].endpoint(
-                    |bot: Bot, msg: Message| async move {
-                        bot.send_message(msg.chat.id, "Start!").await?;
-                        respond(())
-                    },
-                ))
-                .branch(dptree::case![Command::Help].endpoint(
-                    |bot: Bot, msg: Message| async move {
-                        bot.send_message(msg.chat.id, "Help!").await?;
-                        respond(())
-                    },
-                ))
-                .branch(
-                    dptree::case![Command::UpdateEmergencyText]
-                        .enter_dialogue::<Message, InMemStorage<DialogState>, DialogState>()
-                        .endpoint(|bot: Bot, msg: Message, state: MyDialogue| async move {
-                            let res = state.update(DialogState::Start).await;
-                            if res.is_err() {
-                                panic!("{:?}", res.err())
-                            }
-                            bot.send_message(msg.chat.id, "UpdateEmergencyText!")
-                                .await?;
-                            respond(())
-                        }),
-                ),
+            dptree::filter(|command| matches!(command, Some(Command::Start)))
+                .endpoint(print_start_info),
         )
         .branch(
-            dptree::entry()
-                .enter_dialogue::<Message, InMemStorage<DialogState>, DialogState>()
-                .filter_async(|state: MyDialogue| async move { state.get().await.is_ok() })
-                .branch(dptree::case![DialogState::Start].endpoint(
-                    |bot: Bot, state: MyDialogue, msg: Message| async move {
-                        let _ = state.update(DialogState::AwaitingName).await;
-                        bot.send_message(msg.chat.id, "Awaiting name!").await?;
-                        respond(())
-                    },
-                ))
-                .branch(dptree::case![DialogState::AwaitingName].endpoint(
-                    |bot: Bot, state: MyDialogue, msg: Message| async move {
-                        let _ = state.update(DialogState::AwaitingAge).await;
-                        bot.send_message(msg.chat.id, "Awaiting Age!").await?;
-                        respond(())
-                    },
-                ))
-                .branch(dptree::case![DialogState::AwaitingAge].endpoint(
-                    |bot: Bot, state: MyDialogue, msg: Message| async move {
-                        let _ = state.exit().await;
-                        bot.send_message(msg.chat.id, "Finished!").await?;
-                        respond(())
-                    },
-                )),
+            dptree::filter(|command| matches!(command, Some(Command::TestError)))
+                .endpoint(trigger_error),
         )
-        .endpoint(|bot: Bot, msg: Message| async move {
-            bot.send_message(msg.chat.id, "Unknown!").await?;
-            respond(())
+        .branch(
+            dptree::filter(|command| matches!(command, Some(Command::Help)))
+                .endpoint(print_help_info),
+        )
+        .branch(
+            dptree::filter(|command| matches!(command, Some(Command::UpdateEmergencyText)))
+                .endpoint(ask_for_emergency_info),
+        )
+        .branch(
+            dptree::filter(|state: BotDialogState| {
+                matches!(state, BotDialogState::WaitingEmergencyText)
+            })
+            .endpoint(update_emergency_info),
+        )
+        .branch(
+            dptree::filter(|command| matches!(command, Some(Command::GetEmergencyText)))
+                .endpoint(get_emergency_info),
+        )
+        .endpoint(|bot: Bot, message: Message| async move {
+            bot.send_message(message.chat.id, "Unknown command!")
+                .await?;
+            Ok(())
         });
 
     log::info!("Started listening...");
     Dispatcher::builder(bot, handler)
-        .dependencies(dptree::deps![InMemStorage::<DialogState>::new()])
+        .dependencies(dptree::deps![
+            InMemStorage::<BotDialogState>::new(),
+            connection
+        ])
         .enable_ctrlc_handler()
         .build()
         .dispatch()
@@ -138,37 +212,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
 //     let connection = &*connection;
 
 //     match command {
-//         Command::Start | Command::Help => {
-//             let _ = bot
-//                 .send_message(message.chat.id, Command::descriptions().to_string())
-//                 .await;
-//         }
-//         Command::UpdateEmergencyText => {
-//             let _ = dialogue.update(DialogState::Start).await;
-
-//             // let new_emergency_info = emergency_info::ActiveModel {
-//             //     text: ActiveValue::Set(emergency_text),
-//             //     chat_id: ActiveValue::Set(msg.chat.id.0),
-//             //     ..Default::default()
-//             // };
-//             // let res = EmergencyInfo::insert(new_emergency_info)
-//             //     .on_conflict(
-//             //         OnConflict::column(emergency_info::Column::ChatId)
-//             //             .update_column(emergency_info::Column::Text)
-//             //             .to_owned()
-//             //     )
-//             //     .exec(connection)
-//             //     .await;
-
-//             // match res {
-//             //     Ok(_) => bot.send_message(msg.chat.id, format!("Added!")).await?,
-//             //     Err(err) => {
-//             //         error!("Can't add an entry: {:?}", err);
-//             //         bot.send_message(msg.chat.id, format!("Can't update!"))
-//             //             .await?
-//             //     }
-//             // }
-//         }
 //         Command::GetEmergencyText => {
 //             let res = EmergencyInfo::find()
 //                 .filter(emergency_info::Column::ChatId.eq(message.chat.id.0))
